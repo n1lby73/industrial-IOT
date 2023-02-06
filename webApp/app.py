c138bf1b39615384f5b4769cd61c0feff9854336
n1lby73 <n1lby73@gmail.com>
2023-02-06 06:46:33 +0100

    added new routes to the app script

--- webApp/app.py
+++ webApp/app.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "test"
+    return render_template("index.html")
+
+@app.route('/on')
+def on():
+    return render_template("run.html")
+
+@app.route('/btn', methods=['POST', 'GET'])
+def btn():
+    if request.method != 'POST':
+        return redirect(url_for('index'))
+    
+    data = request.get_json()
+    print(data['btnvalue'])
+    # return "work"
+    return jsonify(success=True)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=3565)
