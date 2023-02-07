c8fdaba47768443db1eb0ab9dc786f4a624c851f
n1lby73 <n1lby73@gmail.com>
2023-02-07 16:24:53 +0100

    set nullable value to false

--- webApp/app.py
+++ webApp/app.py
@@ -18,8 +18,8 @@ class esp32(db.Model):
 
     __tablename__ = 'motor'
     id = db.Column(db.Integer, primary_key = True)
-    esp32pin = db.Column(db.String(2), nullable = True, unique = True)
-    switchState = db.Column(db.String(2), nullable = True)
+    esp32pin = db.Column(db.String(2), nullable = False, unique = True)
+    switchState = db.Column(db.String(2), nullable = False)
 
     def __repr__(self):
         return f'<esp32 {self.esp32pin} {self.switchState}>'
