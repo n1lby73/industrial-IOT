b6a6399ddedcce23794b242b2e091efc27981b79
n1lby73 <n1lby73@gmail.com>
2023-03-20 18:07:53 +0100

    tweaked the script to display a msg if esp is offline onload [stability -- poor(not tested)]

--- webApp/static/scripts/index.js
+++ webApp/static/scripts/index.js
@@ -1,7 +1,8 @@
 const button = document.querySelector('#toggleBtn');
 const rotateImage = document.getElementById("rotate");
+const offlineMsg = document.getElementById("onlinie");
 var socket = io.connect('http://127.0.0.1:5000/'); //url currently subject to change
-
+var onlineStatus;
 
 socket.emit("current_status")
 
@@ -137,28 +138,43 @@ const handleButtonClick = () => {
 
 // });
 
-function handleOnLoad(value){
+function handleOnLoad(value, onlineStatus){
+  if (onlineStatus === 1){
 
-  if(value === 1){
+    offlineMsg.classList.remove("online");
 
-    button.innerHTML = "OFF";
-    rotateImage.classList.toggle("rotate");
-    document.title = 'running';
+    if (value === 1){
 
+      button.innerHTML = "OFF";
+      rotateImage.classList.toggle("rotate");
+      document.title = 'running';
+
+    }
+
+    else{
+
+      button.innerHTML = "ON";
+      rotateImage.classList.remove("rotate");
+      document.title = 'Halted';
+      console.log("else");
+    }
   }
 
   else{
-    console.log("else");
-  }
 
+    offlineMsg.classList.toggle("online");
+
+  }
 };
 
 socket.on('message', function(msg){
 
   var value = JSON.parse(msg.success);
+  onlineStatus = JSON.parse(msg.onlineState)
+
   console.log(value);
 
-  handleOnLoad(value);
+  handleOnLoad(value, onlineStatus);
   
   console.log("valuefcmxgfdx ngfgf");
 });
