76b13e8a7feedc04525884f59b9b009619314370
n1lby73 <n1lby73@gmail.com>
2023-04-02 22:43:52 +0100

    crested login form

---
+++ webApp/signinForm.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, Email, EqualTo
+
+class regForm (FlaskForm):
+    
+    email = EmailField('Email', validators=[InputRequired(), Email('Input a valid email')]) 
+
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=8, max=20, message='Password must be  between 8-20 characters')])
+
+    reg = SubmitField("Login")
