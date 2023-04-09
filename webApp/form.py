eb76faad01be9e0f78e009f3572dd0c23b6dafeb
n1lby73 <n1lby73@gmail.com>
2023-04-09 21:30:19 +0100

    added class object for forgotten password (known user)

--- webApp/form.py
+++ webApp/form.py
@@ -1,3 +1,5 @@
+#Fp = forgotten password
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
@@ -9,3 +11,13 @@ class loginForm (FlaskForm):
     password = PasswordField("Password", validators=[InputRequired(), Length(min=8, max=20, message='Password must be  between 8-20 characters')])
 
     reg = SubmitField("Login")
+
+class  knownUserFp(FlaskForm):
+    
+    currentPassword = PasswordField("Current Password", validators=[InputRequired(), Length(min=8, max=20, message='Password must be  between 8-20 characters')]) 
+
+    newPassword = PasswordField("New Password", validators=[InputRequired(), Length(min=8, max=20, message='Password must be  between 8-20 characters')])
+
+    confirmPass = PasswordField("Confirm password", validators=[InputRequired(), EqualTo('newPassword', message="Password must be same")])
+    
+    reset = SubmitField("Reset")
