5d38b14c6cce687a784c090a3b4ce8820ecd8c19
n1lby73 <n1lby73@gmail.com>
2023-04-10 11:49:16 +0100

    re-formated code

--- webApp/app.py
+++ webApp/app.py
@@ -17,17 +17,18 @@ load_dotenv()
 app = Flask(__name__)
 app.app_context().push()
 
+app.config['MAIL_DEFAULT_SENDER'] = formataddr((os.getenv("mail_default_sender_name"), os.getenv("mail_default_sender_email")))
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['MAIL_SERVER'] = os.getenv("mail_server")
-app.config['MAIL_PORT'] = os.getenv("mail_port")
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = os.getenv("mail_use_ssl")
-app.config['MAIL_DEBUG'] = True
 app.config['MAIL_USERNAME'] = os.getenv("mail_username")
 app.config['MAIL_PASSWORD'] = os.getenv("mail_password")
-app.config['MAIL_DEFAULT_SENDER'] = formataddr((os.getenv("mail_default_sender_name"), os.getenv("mail_default_sender_email")))
+app.config['MAIL_USE_SSL'] = os.getenv("mail_use_ssl")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = os.getenv("mail_server")
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['MAIL_PORT'] = os.getenv("mail_port")
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_DEBUG'] = True
+
 
 mail = Mail(app)
 db = SQLAlchemy(app)
