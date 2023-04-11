47ca36e706e1be808f0e651554dfb2c4030209d4
n1lby73 <n1lby73@gmail.com>
2023-04-11 14:16:38 +0100

    created class object for registering

--- webApp/form.py
+++ webApp/form.py
@@ -12,6 +12,18 @@ class loginForm (FlaskForm):
 
     reg = SubmitField("Login")
 
+class regForm (FlaskForm):
+    
+    username = StringField('username', validators=[InputRequired(), Length(min=5, max=20, message='username must be between 5-20 characters')])
+
+    email = EmailField('email', validators=[InputRequired(), Email('Input a valid email')]) 
+
+    password = PasswordField("password", validators=[InputRequired(), EqualTo("confirmpass",message="password must be the same"), Length(min=8, max=20, message='password must be  between 8-20 characters')])
+
+    confirmpass = PasswordField("Confirm password", validators=[InputRequired(), EqualTo('password', message="passwords must be same")])
+
+    reg = SubmitField("sign up")
+
 class  knownUserFp(FlaskForm):
     
     currentPassword = PasswordField("Current Password", validators=[InputRequired(), Length(min=8, max=20, message='Password must be  between 8-20 characters')]) 
