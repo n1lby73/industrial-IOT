1abd14eb65143720fbe4bdfbe46821c2badb6e8e
n1lby73 <n1lby73@gmail.com>
2023-04-11 19:04:01 +0100

    moved configs to a seperate file without causingimport error

--- web-ui/webApp/__init__.py
+++ web-ui/webApp/__init__.py
@@ -2,31 +2,33 @@ from flask_socketio import SocketIO, send, emit
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from email.utils import formataddr
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from flask_mail import Mail
 from flask import Flask
 import os
 
 # from webApp.config import config
 
-load_dotenv()
+# load_dotenv()
 
 app = Flask(__name__)
 app.app_context().push()
 
-app.config['MAIL_DEFAULT_SENDER'] = formataddr((os.getenv("mail_default_sender_name"), os.getenv("mail_default_sender_email")))
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
-app.config['MAIL_USERNAME'] = os.getenv("mail_username")
-app.config['MAIL_PASSWORD'] = os.getenv("mail_password")
-app.config['MAIL_USE_SSL'] = os.getenv("mail_use_ssl")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['MAIL_SERVER'] = os.getenv("mail_server")
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-app.config['MAIL_PORT'] = os.getenv("mail_port")
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_DEBUG'] = True
+# app.config['MAIL_DEFAULT_SENDER'] = formataddr((os.getenv("mail_default_sender_name"), os.getenv("mail_default_sender_email")))
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+# app.config['MAIL_USERNAME'] = os.getenv("mail_username")
+# app.config['MAIL_PASSWORD'] = os.getenv("mail_password")
+# app.config['MAIL_USE_SSL'] = os.getenv("mail_use_ssl")
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['MAIL_SERVER'] = os.getenv("mail_server")
+# app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+# app.config['MAIL_PORT'] = os.getenv("mail_port")
+# app.config['MAIL_USE_TLS'] = False
+# app.config['MAIL_DEBUG'] = True
 
 # app.config.from_object(config['development'])
+
+from webApp import config
 
 db = SQLAlchemy(app)
 mail = Mail(app)
--- web-ui/webApp/config.py
+++ web-ui/webApp/config.py
@@ -1,45 +1,49 @@
 from email.utils import formataddr
-from flask_mail import Mail
+from dotenv import load_dotenv
+from webApp import app
+# from flask_mail import Mail
 import os
 
-# app.config['MAIL_DEFAULT_SENDER'] = formataddr((os.getenv("mail_default_sender_name"), os.getenv("mail_default_sender_email")))
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
-# app.config['MAIL_USERNAME'] = os.getenv("mail_username")
-# app.config['MAIL_PASSWORD'] = os.getenv("mail_password")
-# app.config['MAIL_USE_SSL'] = os.getenv("mail_use_ssl")
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['MAIL_SERVER'] = os.getenv("mail_server")
-# app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-# app.config['MAIL_PORT'] = os.getenv("mail_port")
-# app.config['MAIL_USE_TLS'] = False
-# app.config['MAIL_DEBUG'] = True
+load_dotenv()
 
-class Config:
-    DEBUG = False
-    TESTING = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    MAIL_DEFAULT_SENDER = formataddr((os.getenv("mail_default_sender_name"), os.getenv("mail_default_sender_email")))
-    MAIL_USERNAME = os.getenv("mail_username")
-    MAIL_PASSWORD = os.getenv("mail_password")
-    MAIL_USE_SSL = os.getenv("mail_use_ssl")
-    MAIL_SERVER = os.getenv("mail_server")
-    MAIL_PORT = os.getenv("mail_port")
-    MAIL_USE_TLS = False
-    MAIL_DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+app.config['MAIL_DEFAULT_SENDER'] = formataddr((os.getenv("mail_default_sender_name"), os.getenv("mail_default_sender_email")))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+app.config['MAIL_USERNAME'] = os.getenv("mail_username")
+app.config['MAIL_PASSWORD'] = os.getenv("mail_password")
+app.config['MAIL_USE_SSL'] = os.getenv("mail_use_ssl")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = os.getenv("mail_server")
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['MAIL_PORT'] = os.getenv("mail_port")
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_DEBUG'] = True
 
-class ProductionConfig(Config):
-    pass
+# class Config:
+#     DEBUG = False
+#     TESTING = False
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SECRET_KEY = os.getenv("SECRET_KEY")
+#     MAIL_DEFAULT_SENDER = formataddr((os.getenv("mail_default_sender_name"), os.getenv("mail_default_sender_email")))
+#     MAIL_USERNAME = os.getenv("mail_username")
+#     MAIL_PASSWORD = os.getenv("mail_password")
+#     MAIL_USE_SSL = os.getenv("mail_use_ssl")
+#     MAIL_SERVER = os.getenv("mail_server")
+#     MAIL_PORT = os.getenv("mail_port")
+#     MAIL_USE_TLS = False
+#     MAIL_DEBUG = True
+#     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
 
-class DevelopmentConfig(Config):
-    DEBUG = True
+# class ProductionConfig(Config):
+#     pass
 
-class TestingConfig(Config):
-    TESTING = True
+# class DevelopmentConfig(Config):
+#     DEBUG = True
 
-config = {
-    'production': ProductionConfig,
-    'development': DevelopmentConfig,
-    'testing': TestingConfig
-}
+# class TestingConfig(Config):
+#     TESTING = True
+
+# config = {
+#     'production': ProductionConfig,
+#     'development': DevelopmentConfig,
+#     'testing': TestingConfig
+# }
