1ee5cac06178b2946f4fe19abcc49f123967efae
n1lby73 <n1lby73@gmail.com>
2023-04-12 13:48:24 +0100

    stopped using formataddr import

--- web-ui/webApp/config.py
+++ web-ui/webApp/config.py
@@ -1,12 +1,12 @@
-from email.utils import formataddr
 from dotenv import load_dotenv
 from webApp import app
 import os
 
 load_dotenv()
 
-app.config['MAIL_DEFAULT_SENDER'] = formataddr((os.getenv("mail_default_sender_name"), os.getenv("mail_default_sender_email")))
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+# app.config['MAIL_DEFAULT_SENDER'] = formataddr((os.getenv("mail_default_sender_name"), os.getenv("mail_default_sender_email")))
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('mail_default_sender')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['MAIL_USERNAME'] = os.getenv("mail_username")
 app.config['MAIL_PASSWORD'] = os.getenv("mail_password")
 app.config['MAIL_USE_SSL'] = os.getenv("mail_use_ssl")
