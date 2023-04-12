ba07bbbb4aed2b1ab8168bb51fa51669b0e157bd
n1lby73 <n1lby73@gmail.com>
2023-04-12 18:15:14 +0100

    delted all commented code regarding emial verification

--- web-ui/webApp/routes.py
+++ web-ui/webApp/routes.py
@@ -19,11 +19,6 @@ espstate = 0
 startTime = 0
 timeout = 2
 
-# email = " "
-# username = " " 
-# password = " " 
-# otp = " "
-
 @login.user_loader
 def load_user(user_id):
     return users.query.get(int(user_id))
@@ -40,8 +35,6 @@ def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     
-    # global email, username, password, otp
-
     form=regForm()
     emailForm = confirmEmail()
 
@@ -57,46 +50,13 @@ def register():
         db.session.commit()
 
         login_user(new_user)
-
         next_page = request.args.get('next')
 
         if not next_page or url_parse(next_page).netloc != '':
 
             next_page = url_for('index')
+
         return redirect(next_page)
-
-    #     otp = 123456
-    #     msg = Message('Email OTP verification', recipients=[email])
-    #     msg.body = render_template("emailVerification.txt", otp=otp)
-    #     mail.send(msg)
-    #     # return render_template("unKnownUserFp.html", form=unKnownUserForm)
-    #     return render_template("confirmEmail.html", form=emailForm)
-    
-    # if emailForm.validate_on_submit:
-
-    #     confirmOTP = request.form.get('emailOTP')
-    #     print(confirmOTP)
-    #     if confirmOTP == otp:
-    #         print("gjyegwygyftwyukkfyufgewje")
-    #         new_user = users(email=email, password=generate_password_hash(password), username=username, role="user")
-
-    #         db.session.add(new_user)
-    #         db.session.commit()
-
-    #         login_user(new_user)
-    #         next_page = request.args.get('next')
-
-    #         if not next_page or url_parse(next_page).netloc != '':
-
-    #             next_page = url_for('index')
-    #             return redirect(next_page)
-            
-    #         return redirect(url_for('index'))
-
-    #     else:
-
-    #         flash("Invalid OTP")
-    #         return render_template("confirmEmail.html", form=emailForm)
 
     return render_template("signup.html", form=form)
 
