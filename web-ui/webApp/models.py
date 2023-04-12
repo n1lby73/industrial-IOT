531586602c1c821366f96e8885bcc73281a9acfc
n1lby73 <n1lby73@gmail.com>
2023-04-12 17:56:23 +0100

    removed otp column

--- web-ui/webApp/models.py
+++ web-ui/webApp/models.py
@@ -18,7 +18,6 @@ class users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(150), nullable = False, unique = True)
     email = db.Column(db.String(150), nullable = False, unique = True)
-    otp = db.Column(db.String(150), nullable = True, unique = True)
     role = db.Column(db.String(150), nullable = False)
     password = db.Column(db.String(150), nullable = False)
     
