d1a915b3c3bd82b3cedbb4e3254e2942d56efedb
n1lby73 <n1lby73@gmail.com>
2023-04-12 13:49:15 +0100

    removed commented lines

--- web-ui/webApp/config.py
+++ web-ui/webApp/config.py
@@ -4,7 +4,6 @@ import os
 
 load_dotenv()
 
-# app.config['MAIL_DEFAULT_SENDER'] = formataddr((os.getenv("mail_default_sender_name"), os.getenv("mail_default_sender_email")))
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('mail_default_sender')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['MAIL_USERNAME'] = os.getenv("mail_username")
