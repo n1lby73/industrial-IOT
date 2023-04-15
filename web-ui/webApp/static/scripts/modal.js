1cff8f6755682dd95939eb84032f66b4e99b1f97
n1lby73 <n1lby73@gmail.com>
2023-04-16 00:10:34 +0100

    added confirmation moodal

--- web-ui/webApp/static/scripts/modal.js
+++ web-ui/webApp/static/scripts/modal.js
@@ -4,6 +4,56 @@ const closeModal = document.querySelector(".close");
 const searchInput = document.getElementById("user-search");
 const rows = document.querySelectorAll("tbody tr");
 
+// Get the confirmation modal
+var confirmModal = document.getElementById("confirmModal");
+
+// Get the cancel and save buttons
+var cancelBtn = confirmModal.querySelector(".cancel-button");
+var saveBtn = confirmModal.querySelector(".save-button");
+
+// Get all the messages in the dropdown
+var messages = document.querySelectorAll(".message");
+
+// When a message is clicked
+messages.forEach(function(message) {
+  message.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Show the confirmation modal
+    confirmModal.style.display = "block";
+  });
+});
+
+// When the cancel button is clicked
+cancelBtn.addEventListener("click", function() {
+  // Hide the confirmation modal
+  confirmModal.style.display = "none";
+});
+
+// When the save button is clicked
+saveBtn.addEventListener("click", function() {
+  // Do something here (e.g. submit a form)
+
+  // Hide the confirmation modal
+  confirmModal.style.display = "none";
+});
+
+// When the user clicks on the close button of the modals
+var closeButtons = document.querySelectorAll(".close");
+closeButtons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    // Hide the modal
+    var modal = button.parentElement.parentElement;
+    modal.style.display = "none";
+  });
+});
+
+window.addEventListener("click", function(event) {
+    if (event.target == confirmModal) {
+      confirmModal.style.display = "none";
+    }
+  });
+
 usersLink.addEventListener("click", function() {
     event.preventDefault();
     userModal.style.display = "block";
