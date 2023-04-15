9a65e5e9b8c846cf031cf5baea9c1f860a9c00b0
n1lby73 <n1lby73@gmail.com>
2023-04-15 22:58:12 +0100

    worked on at least a presentable styling

--- web-ui/webApp/static/scripts/modal.js
+++ web-ui/webApp/static/scripts/modal.js
@@ -1,6 +1,8 @@
 const usersLink = document.getElementById("users");
 const userModal = document.getElementById("user-modal");
 const closeModal = document.querySelector(".close");
+const searchInput = document.getElementById("user-search");
+const rows = document.querySelectorAll("tbody tr");
 
 usersLink.addEventListener("click", function() {
     event.preventDefault();
@@ -16,4 +18,18 @@ window.addEventListener("click", function(event) {
     if (event.target === userModal) {
         userModal.style.display = "none";
     }
+});
+
+searchInput.addEventListener("keyup", function(event) {
+    const query = event.target.value.toLowerCase();
+
+    rows.forEach(row => {
+        const text = row.textContent.toLowerCase();
+
+        if (text.includes(query)) {
+            row.style.display = "table-row";
+        } else {
+            row.style.display = "none";
+        }
+    });
 });
