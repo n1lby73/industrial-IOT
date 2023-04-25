80ad77f9047cdd15c1413eeba4fef8ad79e8c2a6
n1lby73 <n1lby73@gmail.com>
2023-04-25 11:24:10 +0100

    removed host and debug

--- web-ui/run.py
+++ web-ui/run.py
@@ -1,4 +1,5 @@
 from webApp import app, socketio
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True)
+    socketio.run(app)
+    # socketio.run(app, host='0.0.0.0', debug=True)
