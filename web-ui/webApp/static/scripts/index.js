c1744953fa459a1b2be497e54bb9c2e289d6dc26
n1lby73 <n1lby73@gmail.com>
2023-04-27 22:13:52 +0100

    added content in newly created role and index js files

---
+++ web-ui/webApp/static/scripts/index.js
@@ -0,0 +1,193 @@
+const button = document.querySelector('#toggleBtn');
+const rotateImage = document.getElementById("rotate-image");
+const offlineMsg = document.getElementById("online");
+
+const socket = new WebSocket('ws://127.0.0.1:5000');
+
+var onlineStatus;
+
+offlineMsg.style.display = "none";
+
+
+socket.addEventListener('open', (event) => {
+
+    socket.emit("current_status")
+    socket.emit("espstatus")
+    console.log('WebSocket connection opened');
+    
+});
+
+// socket.on("connect", function() {
+
+//     socket.emit("current_status")
+//     socket.emit("espstatus")
+
+// });
+
+const handleButtonClick = () => {
+
+  if (onlineStatus === 1){
+
+    offlineMsg.classList.remove("online");
+
+    let data = {};
+
+    if (button.innerHTML === "ON") {
+
+      button.innerHTML = "OFF";
+
+      data = { 
+        
+        state: 1,
+        pin : 5
+
+      };
+
+      rotateImage.classList.toggle("rotate");
+      console.log("tuygjyfg kxqkJG CB UF GCJGuy g BCHJ")
+      document.title = 'Running'
+
+      socket.emit('update', data);
+
+    } 
+    
+    else {
+
+      button.innerHTML = "ON";
+      data = {
+
+        state: 0,
+        pin : 5
+        
+      };
+
+      rotateImage.classList.remove("rotate");
+      document.title = 'Halted'
+
+      socket.emit('update', data)
+
+    }
+  }
+  
+  else{
+
+    offlineMsg.style.display = "inline";
+    document.title = 'Offline';
+
+    }
+};
+
+function handleOnLoad(value, onlineStatus){
+  if (onlineStatus === 1){
+
+    offlineMsg.classList.remove("online");
+
+    if (value === 1){
+
+      button.innerHTML = "OFF";
+      rotateImage.classList.toggle("rotate");
+      document.title = 'Running';
+
+    }
+
+    else{
+
+      button.innerHTML = "ON";
+      rotateImage.classList.remove("rotate");
+      document.title = 'Halted';
+      console.log("else");
+    }
+  }
+
+  else{
+
+    offlineMsg.style.display = "inline";
+    document.title = 'Offline';
+
+  }
+};
+
+socket.addEventListener('message', (msg) => {
+
+    var value = JSON.parse(msg.success);
+
+    onlineStatus = msg.value;
+
+    handleOnLoad(value, onlineStatus);
+    console.log(`Received message: ${msg.success}`);
+
+});
+
+// socket.on('message', function(msg){
+
+//   var value = JSON.parse(msg.success);
+
+//   onlineStatus = msg.value;
+
+//   handleOnLoad(value, onlineStatus);
+  
+// });
+
+socket.addEventListener('localUpdate', (msg) => {
+
+    var update = JSON.parse(msg.update);
+  
+    console.log(update);
+
+    handleOnLoad(update, 1);
+    
+    console.log("syncupdate");
+
+    console.log(`Received message: ${msg.update}`);
+});
+
+
+// socket.on('localUpdate', function(msg){
+
+//   var update = JSON.parse(msg.update);
+  
+//   console.log(update);
+
+//   handleOnLoad(update, 1);
+  
+//   console.log("syncupdate");
+// });
+
+socket.addEventListener('message', (msg) => {
+
+    onlineStatus = msg.value;
+
+    if (onlineStatus === 0){
+  
+      offlineMsg.style.display = "inline";
+      document.title = 'Offline';
+  
+    }
+  
+    else{
+  
+      offlineMsg.style.display = "none";
+      
+    }
+    console.log(`Received message: ${mg.value}`);
+  });
+
+// socket.on('espOnlineState', function(msg){
+
+//   onlineStatus = msg.value;
+
+//   if (onlineStatus === 0){
+
+//     offlineMsg.style.display = "inline";
+//     document.title = 'Offline';
+
+//   }
+
+//   else{
+
+//     offlineMsg.style.display = "none";
+    
+//   }
+// })
+
+button.addEventListener('click', handleButtonClick);
---
+++ web-ui/webApp/static/scripts/role.js
@@ -0,0 +1,80 @@
+const socket = new WebSocket('ws://127.0.0.1:5000');
+
+
+var role;
+
+document.getElementById('loading-screen').style.display = 'block';
+
+socket.addEventListener('connect', (event) => {
+
+    socket.emit("role");
+
+});
+
+// socket.on("connect", function() {
+//   console.log("immediately after connect socket");
+//   socket.emit("role");
+//   console.log("role inside socket");
+// });
+
+console.log("role outside socket");
+
+socket.addEventListener('storeRole', (msg) => {
+
+    role = msg.role;
+   
+    if(role === "user"){
+
+        document.querySelector('#toggleBtn').style.display = "none";
+        document.querySelector('#users').style.display = "none";
+
+    }
+
+    else if(role === "admin"){
+
+        document.querySelector('#toggleBtn').style.display = "inline-block";
+        document.querySelector('#users').style.display = "none";
+
+    }
+
+    else{
+
+        document.querySelector('#toggleBtn').style.display = "inline-block";
+        document.querySelector('#users').style.display = "inline-block";
+
+    }
+
+    document.getElementById('loading-screen').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+
+});
+
+// socket.on('storeRole', function(msg){
+
+//   role = msg.role;
+   
+//   if(role === "user"){
+
+//     document.querySelector('#toggleBtn').style.display = "none";
+//     document.querySelector('#users').style.display = "none";
+
+//   }
+
+//   else if(role === "admin"){
+
+//     document.querySelector('#toggleBtn').style.display = "inline-block";
+//     document.querySelector('#users').style.display = "none";
+
+//   }
+
+//   else{
+
+//     document.querySelector('#toggleBtn').style.display = "inline-block";
+//     document.querySelector('#users').style.display = "inline-block";
+
+//   }
+
+//   document.getElementById('loading-screen').style.display = 'none';
+//   document.getElementById('content').style.display = 'block';
+
+// });
