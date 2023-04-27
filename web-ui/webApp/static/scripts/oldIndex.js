ac0c40fa21b201cf6e78781eb4be0dcca5c4bbf7
n1lby73 <n1lby73@gmail.com>
2023-04-27 21:32:11 +0100

    creating new index and role js file

---
+++ web-ui/webApp/static/scripts/oldIndex.js
@@ -0,0 +1,161 @@
+const button = document.querySelector('#toggleBtn');
+const rotateImage = document.getElementById("rotate-image");
+const offlineMsg = document.getElementById("online");
+
+var socket = io.connect('http://127.0.0.1:5000/'); //url currently subject to change
+// var socket = io('http://192.168.0.145:5000/', {
+//     cors: {
+//         origin: '*',
+//         methods: ['GET', 'POST']
+//     }
+// });
+// var socket = io.connect('https://' + window.location.hostname + ':' + location.port);
+// var socket = io.connect('https://industrialiot.onrender.com/');
+// var socket = io.connect('https://216.24.57.253:443');
+// var socket= io.connect('https://industrialiot.onrender.com/', {
+//       cors: {
+//           origin: '*',
+//           methods: ['GET', 'POST']
+//       }
+//   })
+
+var onlineStatus;
+console.log("index");
+offlineMsg.style.display = "none";
+
+// socket.emit("current_status")
+// socket.emit("espstatus")
+
+
+socket.on("connect", function() {
+  // socket.emit("role");
+  socket.emit("current_status")
+  socket.emit("espstatus")
+
+});
+
+const handleButtonClick = () => {
+
+  if (onlineStatus === 1){
+
+    offlineMsg.classList.remove("online");
+
+    let data = {};
+
+    if (button.innerHTML === "ON") {
+
+      button.innerHTML = "OFF";
+
+      data = { 
+        
+        state: 1,
+        pin : 5
+
+      };
+
+      rotateImage.classList.toggle("rotate");
+      console.log("tuygjyfg kxqkJG CB UF GCJGuy g BCHJ")
+      document.title = 'Running'
+
+      socket.emit('update', data);
+
+    } 
+    
+    else {
+
+      button.innerHTML = "ON";
+      data = {
+
+        state: 0,
+        pin : 5
+        
+      };
+
+      rotateImage.classList.remove("rotate");
+      document.title = 'Halted'
+
+      socket.emit('update', data)
+
+    }
+  }
+  
+  else{
+
+    offlineMsg.style.display = "inline";
+    document.title = 'Offline';
+
+    }
+};
+
+function handleOnLoad(value, onlineStatus){
+  if (onlineStatus === 1){
+
+    offlineMsg.classList.remove("online");
+
+    if (value === 1){
+
+      button.innerHTML = "OFF";
+      rotateImage.classList.toggle("rotate");
+      document.title = 'Running';
+
+    }
+
+    else{
+
+      button.innerHTML = "ON";
+      rotateImage.classList.remove("rotate");
+      document.title = 'Halted';
+      console.log("else");
+    }
+  }
+
+  else{
+
+    offlineMsg.style.display = "inline";
+    document.title = 'Offline';
+
+  }
+};
+
+socket.on('message', function(msg){
+
+  var value = JSON.parse(msg.success);
+  // onlineStatus = JSON.parse(msg.value);
+  onlineStatus = msg.value;
+
+  handleOnLoad(value, onlineStatus);
+  
+});
+
+socket.on('localUpdate', function(msg){
+
+  var update = JSON.parse(msg.update);
+  
+  console.log(update);
+
+  handleOnLoad(update, 1);
+  
+  console.log("syncupdate");
+});
+
+socket.on('espOnlineState', function(msg){
+
+  // console.log(msg.value);
+  // onlineStatus = JSON.parse(msg.value);
+  onlineStatus = msg.value;
+
+  if (onlineStatus === 0){
+
+    offlineMsg.style.display = "inline";
+    document.title = 'Offline';
+
+  }
+
+  else{
+
+    offlineMsg.style.display = "none";
+    
+  }
+})
+
+button.addEventListener('click', handleButtonClick);
---
+++ web-ui/webApp/static/scripts/oldRole.js
@@ -0,0 +1,57 @@
+// var socket = io.connect('http://127.0.0.1:5000/'); //url currently subject to change
+var socket = io.connect('https://' + window.location.hostname+ ':' + location.port);
+// var socket = io('http://192.168.0.145:5000/', {
+//     cors: {
+//         origin: '*',
+//         methods: ['GET', 'POST']
+//     }
+// });
+// var socket= io.connect('https://industrialiot.onrender.com/', {
+//   cors: {
+//       origin: '*',
+//       methods: ['GET', 'POST']
+//   }
+// })
+
+var role;
+
+document.getElementById('loading-screen').style.display = 'block';
+// console.dir(io)
+
+// socket.emit("role")
+
+socket.on("connect", function() {
+  console.log("immediately after connect socket");
+  socket.emit("role");
+  console.log("role inside socket");
+});
+console.log("role outside socket");
+socket.on('storeRole', function(msg){
+
+  role = msg.role;
+   
+  if(role === "user"){
+
+    document.querySelector('#toggleBtn').style.display = "none";
+    document.querySelector('#users').style.display = "none";
+
+  }
+
+  else if(role === "admin"){
+
+    document.querySelector('#toggleBtn').style.display = "inline-block";
+    document.querySelector('#users').style.display = "none";
+
+  }
+
+  else{
+
+    document.querySelector('#toggleBtn').style.display = "inline-block";
+    document.querySelector('#users').style.display = "inline-block";
+
+  }
+
+  document.getElementById('loading-screen').style.display = 'none';
+  document.getElementById('content').style.display = 'block';
+
+});
