ebc790ad78da4ea0da8ef86b8ca57cad44f139bd
n1lby73 <n1lby73@gmail.com>
2023-04-28 07:35:57 +0100

    testing changges on deployed version before making new ones

--- web-ui/webApp/static/scripts/index.js
+++ web-ui/webApp/static/scripts/index.js
@@ -2,7 +2,8 @@ const button = document.querySelector('#toggleBtn');
 const rotateImage = document.getElementById("rotate-image");
 const offlineMsg = document.getElementById("online");
 
-const socket = io('http://127.0.0.1:5000');
+// const socket = io('http://127.0.0.1:5000');
+const socket = io('https://industrialiot.onrender.com');
 
 var onlineStatus;
 
