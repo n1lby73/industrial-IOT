4f1d9a3d9a80fdc6aa094ea5ded7ef31421ad46a
n1lby73 <n1lby73@gmail.com>
2023-06-06 19:51:47 +0100

    working on index api

--- web-ui/webApp/apiRoute.py
+++ web-ui/webApp/apiRoute.py
@@ -1,8 +1,23 @@
+from flask_login import current_user, login_user, logout_user
 from flask_restful import Resource
+from webApp import login
 from webApp import api
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+@login.user_loader
+def load_user(user_id):
+    return users.query.get(int(user_id))
 
-api.add_resource(HelloWorld, '/api')
+class indexApi(Resource):
+    def get(self):
+        if current_user.is_authenticated:
+            return {'hello': 'world'}
+        return "nah"
+
+class loginApi(Resource):
+    def get(self):
+      return "X"
+    def post(self):
+        return "T"
+
+api.add_resource(loginApi, '/api/login')
+api.add_resource(indexApi, '/api')
