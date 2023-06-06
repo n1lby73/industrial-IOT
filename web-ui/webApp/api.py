65ed02fec9cce1a9782c13edc03b615d715396a9
n1lby73 <65239245+n1lby73@users.noreply.github.com>
2023-06-06 15:46:36 +0100

    Modified import and resource route

--- web-ui/webApp/api.py
+++ web-ui/webApp/api.py
@@ -1,7 +1,8 @@
-from webApp import Resource, api
+from flask_restful import Resource
+from webApp import api
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(HelloWorld, '/api')
