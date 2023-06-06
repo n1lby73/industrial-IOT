a68ad34f4315676efeac931e713055916b300fd9
n1lby73 <n1lby73@gmail.com>
2023-06-06 20:35:42 +0100

    working on login api

--- web-ui/webApp/apiRoute.py
+++ web-ui/webApp/apiRoute.py
@@ -1,5 +1,6 @@
 from flask_login import current_user, login_user, logout_user
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from flask import request
 from webApp import login
 from webApp import api
 
@@ -11,13 +12,29 @@ class indexApi(Resource):
     def get(self):
         if current_user.is_authenticated:
             return {'hello': 'world'}
-        return "nah"
+
+        return "user not authorized"
 
 class loginApi(Resource):
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument("email", required=True)
+        self.parser.add_argument("password", required=True)
+    
     def get(self):
-      return "X"
+        return "Error: Method not allowed"
+    
     def post(self):
-        return "T"
+        args = self.parser.parse_args()
+        extra_params = set(request.json.keys()) - set(self.parser.args.keys())
+        if extra_params:
+            error_message = f"Unexpected parameters: {', '.join(extra_params)}"
+            return {"error": error_message}, 400
+
+        email = args["email"]
+        password = args["password"]
+        # Add your login logic here
+        return "The login API was called"
 
 api.add_resource(loginApi, '/api/login')
 api.add_resource(indexApi, '/api')
