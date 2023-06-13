b7fd1459ed9365b4a6bfd356d11bb8dd5c06c52b
n1lby73 <n1lby73@gmail.com>
2023-06-13 10:21:09 +0100

    created register route

--- web-ui/webApp/apiRoute.py
+++ web-ui/webApp/apiRoute.py
@@ -2,7 +2,7 @@ from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_requir
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_restful import Resource, reqparse
 from webApp.models import users, esp32
-from webApp import api, jwt
+from webApp import api, jwt, db
 from flask import jsonify
 
 class indexApi(Resource):
@@ -26,6 +26,9 @@ class loginApi(Resource):
     
     def post(self):
 
+        if get_jwt_identity():
+            return {"msg: A user is already logged in"}
+        
         args = self.parser.parse_args()
         email = args["email"]
         password = args["password"]
@@ -38,5 +41,48 @@ class loginApi(Resource):
         access_token = create_access_token(identity=email)
         return jsonify(access_token=access_token)
 
-api.add_resource(loginApi, '/api/login')
+class registerApi(Resource):
+
+    def __init__(self):
+
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument("email", required=True)
+        self.parser.add_argument("username", required=True)
+        self.parser.add_argument("password", required=True)
+        self.parser.add_argument("confirmPassword", required=True)
+    
+    def get(self):
+
+        return ({"msg": "Method not allowed"}), 405
+    
+    def post(self):
+
+        args = self.parser.parse_args()
+        email = args["email"]
+        username = args["username"]
+        password = args["password"]
+        password2 = args["confirmPassword"]
+
+        existingUserName = users.query.filter_by(username=username).first()
+        existingMail = users.query.filter_by(email=email.data).first()
+
+        if existingUserName:
+            return {"Error: Username is already taken"}
+        
+        if existingMail:
+            return {"Error: Email already exit"}
+        
+        if password != password2:
+            return {"error: password do not match"}
+        
+        new_user = users(email=email, username=username, role="user", password=generate_password_hash(password))
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return {"Sucess: new user created"}
+    
 api.add_resource(indexApi, '/api')
+api.add_resource(loginApi, '/api/login')
+api.add_resource(registerApi, '/api/register')
+
