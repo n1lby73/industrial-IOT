faba12d3c25cbee7118982268767425d0b356a35
n1lby73 <n1lby73@gmail.com>
2023-06-15 16:21:18 +0100

    allowed all origin

--- web-ui/webApp/__init__.py
+++ web-ui/webApp/__init__.py
@@ -20,8 +20,8 @@ mail = Mail(app)
 jwt = JWTManager(app)
 login = LoginManager()
 migrate = Migrate(app, db)
-# socketio = SocketIO(app, cors_allowed_origins='*')
-socketio = SocketIO(app, cors_allowed_origins='127.0.0.1:5000')
+socketio = SocketIO(app, cors_allowed_origins='*')
+# socketio = SocketIO(app, cors_allowed_origins='127.0.0.1:5000')
 #sio = socketio.AsyncServer()
 
 from webApp import routes, apiRoute
