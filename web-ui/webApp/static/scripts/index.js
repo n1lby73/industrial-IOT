389daedd1c5974c0e5ea1a863e8903ac30f7698f
n1lby73 <n1lby73@gmail.com>
2023-06-22 07:53:01 +0100

    checking for valid socket to connect to

--- web-ui/webApp/static/scripts/index.js
+++ web-ui/webApp/static/scripts/index.js
@@ -4,9 +4,9 @@ const offlineMsg = document.getElementById("online");
 
 // const socket = io('http://127.0.0.1:5000'); 
 // const socket = io.connect( {cookie: false}, 'https://industrialiot.onrender.com');
-const socket = io.connect('https://industrialiot.onrender.com');
+// const socket = io.connect('https://industrialiot.onrender.com');
 // const socket = io('industrialiot.onrender.com');
-// const socket = io('https://industrialiot.onrender.com');
+const socket = io('https://industrialiot.onrender.com');
 
 socket.on('connect', () => {
 
