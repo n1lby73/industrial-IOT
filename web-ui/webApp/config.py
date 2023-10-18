2b436c6e5ea5ac30751fa4ed37f1077aae9de4b8
n1lby73 <n1lby73@gmail.com>
2023-10-18 06:55:25 +0100

    specified access access token expiration time

--- web-ui/webApp/config.py
+++ web-ui/webApp/config.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from datetime import timedelta
 from webApp import app
 import os
 
@@ -7,6 +8,7 @@ load_dotenv()
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('mail_default_sender')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['CACHE_DEFAULT_TIMEOUT'] = int(os.getenv("CACHE_DEFAULT_TIMEOUT"))
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 app.config['CACHE_TYPE'] = os.getenv("CACHE_TYPE")
 app.config['MAIL_USERNAME'] = os.getenv("mail_username")
 app.config['MAIL_PASSWORD'] = os.getenv("mail_password")
@@ -15,5 +17,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = os.getenv("mail_server")
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.config['MAIL_PORT'] = os.getenv("mail_port")
+# app.config["JWT_COOKIE_SECURE"] = True
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_DEBUG'] = True
+# app.config["JWT_TOKEN_LOCATION"] = "cookies"
