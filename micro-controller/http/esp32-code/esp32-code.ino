ad83c39d70b6613ea4c5292ef78318126bde3ec7
n1lby73 <n1lby73@gmail.com>
2023-11-20 23:02:07 +0100

    changed server ip

--- micro-controller/http/esp32-code/esp32-code.ino
+++ micro-controller/http/esp32-code/esp32-code.ino
@@ -23,7 +23,7 @@ int globalState;
 #define password "forTheLoveOfEmbededSystem"
 
 const char* serverID = "industrialiot.onrender.com";
-const char* serverIP = "192.168.0.4"; //host subject to change always untill app is hosted
+const char* serverIP = "192.168.62.228"; //host subject to change always untill app is hosted
 const int serverPort = 5000;
 
 // Ping google.com to know if connected wifi has access to internet
