f9d027827e8739652cd103a5e427d233a2d5cb2a
n1lby73 <n1lby73@gmail.com>
2023-11-26 21:30:15 +0100

    merging frontend to avoid cors

--- web-ui/webApp/__init__.py
+++ web-ui/webApp/__init__.py
@@ -20,7 +20,7 @@ mail = Mail(app)
 jwt = JWTManager(app)
 login = LoginManager()
 migrate = Migrate(app, db)
-socketio = SocketIO(app, cors_allowed_origins=['http://127.0.0.1:5000', 'https://impedo.serveo.net', 'wss://industrialiot.onrender.com', 'https://industrialiot.onrender.com', 'localhost:3000', 'serveo.net:4565'])
+socketio = SocketIO(app, cors_allowed_origins=['http://127.0.0.1:5000', 'https://impedo.serveo.net', 'wss://industrialiot.onrender.com', 'https://industrialiot.onrender.com', 'http://localhost:3000', 'serveo.net:4565'])
 # socketio = SocketIO(app, cors_allowed_origins='*',  transports=['websocket', 'xhr-polling'])
 
 
