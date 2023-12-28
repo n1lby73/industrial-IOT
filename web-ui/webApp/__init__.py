6b299835f8583d3d897904affdf252f625955918
n1lby73 <n1lby73@gmail.com>
2023-12-28 23:26:15 +0100

    allowing all origin for debug purpose

--- web-ui/webApp/__init__.py
+++ web-ui/webApp/__init__.py
@@ -20,19 +20,25 @@ mail = Mail(app)
 jwt = JWTManager(app)
 login = LoginManager()
 migrate = Migrate(app, db)
-socketio = SocketIO(app, cors_allowed_origins=['http://127.0.0.1:5000','http://127.0.0.1:5500', '127.0.0.1:5500' 'https://impedo.serveo.net', 'wss://industrialiot.onrender.com', 'https://industrialiot.onrender.com', 'http://localhost:3000', 'localhost:3000', 'http://serveo.net:4565', 'serveo.net:4565', 'https://4nscf0m4-5500.uks1.devtunnels.ms'])
+# socketio = SocketIO(app, cors_allowed_origins=['http://127.0.0.1:5000','http://127.0.0.1:5500', '127.0.0.1:5500' 'https://impedo.serveo.net', 'wss://industrialiot.onrender.com', 'https://industrialiot.onrender.com', 'http://localhost:3000', 'localhost:3000', 'http://serveo.net:4565', 'serveo.net:4565', 'https://4nscf0m4-5500.uks1.devtunnels.ms'])
+# # socketio = SocketIO(app, cors_allowed_origins='*',  transports=['websocket', 'xhr-polling'])
+
+# # Use CORS for regular HTTP routes
+# CORS(app, resources={r"/api/*": {"origins": [
+#     'http://127.0.0.1:5000',
+#     'http://127.0.0.1:5500',
+#     'http://impedo.serveo.net',
+#     'https://industrialiot.onrender.com',
+#     'http://localhost:3000',
+#     'http://localhost:5173',
+#     'https://vermillion-mooncake-c13f76.netlify.app', #dev dannydoc
+#     'http://serveo.net:4565'
+# ]}})
+
+socketio = SocketIO(app, cors_allowed_origins="*")
 # socketio = SocketIO(app, cors_allowed_origins='*',  transports=['websocket', 'xhr-polling'])
 
 # Use CORS for regular HTTP routes
-CORS(app, resources={r"/api/*": {"origins": [
-    'http://127.0.0.1:5000',
-    'http://127.0.0.1:5500',
-    'http://impedo.serveo.net',
-    'https://industrialiot.onrender.com',
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'https://vermillion-mooncake-c13f76.netlify.app', #dev dannydoc
-    'http://serveo.net:4565'
-]}})
+CORS(app, resources={r"/api/*": {"origins": ["*"]}})
 
 from webApp import routes, apiRoute, socket
